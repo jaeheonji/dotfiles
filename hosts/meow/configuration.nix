b1ba954bc4374a028a37ad9ff8a328503d770a61
Jae-Heon Ji <atx6419@gmail.com>
2023-03-24 17:30:07 +0000

    :wrench: Update hostPlatform

--- hosts/meow/configuration.nix
+++ hosts/meow/configuration.nix
@@ -9,10 +9,13 @@
   ];
 
   nixpkgs = {
+    hostPlatform = "x86_64_linux";
+
     overlays = [
       outputs.overlays.additions
       outputs.overlays.modifications
     ];
+
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
