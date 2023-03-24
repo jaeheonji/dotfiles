6c37f8c82fce855f999133c05ccba7693b104e7d
Jae-Heon Ji <atx6419@gmail.com>
2023-03-24 17:13:17 +0000

    :bug: Fix missing comma

--- hosts/meow/configuration.nix
+++ hosts/meow/configuration.nix
@@ -1,7 +1,7 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 
-{ inputs, outputs, lib, config, pkgs, modulesPath ... }: {
+{ inputs, outputs, lib, config, pkgs, modulesPath, ... }: {
   imports = [
     inputs.nixos-wsl.nixosModules.wsl
 
