04dffcd1ab2cdb17eb4c351119d7e26c686e49d3
Jae-Heon Ji <atx6419@gmail.com>
2023-03-30 14:29:45 +0000

    :arrow_up: Upgrade flake.lock

--- shell.nix
+++ shell.nix
@@ -5,6 +5,9 @@
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [ nix home-manager git ];
+    nativeBuildInputs = with pkgs; [
+      git
+      nodePackages.gitmoji-cli
+    ];
   };
 }
