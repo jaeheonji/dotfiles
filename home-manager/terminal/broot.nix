2b6ea0d052d10ce8b5a02cb3f586b1d79a880de2
Jae-Heon Ji <atx6419@gmail.com>
2023-04-08 15:48:31 +0000

    :wrench: Update broot config

--- home-manager/terminal/broot.nix
+++ home-manager/terminal/broot.nix
@@ -2,5 +2,19 @@
   programs.broot = {
     enable = true;
     enableFishIntegration = true;
+
+    settings = {
+      modal = true;
+
+      verbs = [
+        {
+          invocation = "edit";
+          shortcut = "e";
+          key = "ctrl-e";
+          execution = "$EDITOR {file}";
+          leave_broot = false;
+        }
+      ];
+    };
   };
 }
