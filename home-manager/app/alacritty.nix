8f665fe75714c0b54bbd471e2e3a97c095e76673
Jae-Heon Ji <atx6419@gmail.com>
2023-04-16 14:26:46 +0900

    :wrench: Update alacritty config

--- home-manager/app/alacritty.nix
+++ home-manager/app/alacritty.nix
@@ -1,5 +1,28 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  catppuccin-mocha = ".config/alacritty/catppuccin/catppuccin-mocha.yml";
+in {
   programs.alacritty = {
     enable = true;
+
+    settings = {
+      import = ["~/${catppuccin-mocha}"];
+
+      env.TERM = "xterm-256color";
+
+      window = {
+        decorations = "none";
+        option_as_alt = "OnlyLeft";
+      };
+
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  home.file."${catppuccin-mocha}" = {
+    source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/alacritty/main/catppuccin-mocha.yml";
+      hash = "sha256-28Tvtf8A/rx40J9PKXH6NL3h/OKfn3TQT1K9G8iWCkM=";
+    };
   };
 }
