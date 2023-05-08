a82a626cbc5547724bce10e3747268a79fb98dff
Jae-Heon Ji <atx6419@gmail.com>
2023-05-08 17:12:47 +0000

    :wrench: Add navi cheats

--- home-manager/terminal/navi.nix
+++ home-manager/terminal/navi.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }: {
+  home.file.".local/share/navi" = {
+    source = ../../config/navi;
+    recursive = true;
+  };
+
   programs.navi = {
     enable = true;
     enableFishIntegration = true;
