ec7bb2887e063ad88bd5a54b4be4a2a6354d243d
Jae-Heon Ji <atx6419@gmail.com>
2023-05-14 07:12:06 +0000

    :wrench: Add starship theme

--- home-manager/shell/starship.nix
+++ home-manager/shell/starship.nix
@@ -1,6 +1,23 @@
 { pkgs, ... }: {
-  programs.starship = {
-    enable = true;
-    enableFishIntegration = true;
-  };
+  programs.starship =
+    let
+      flavour = "mocha";
+    in
+    {
+      enable = true;
+      enableFishIntegration = true;
+
+      settings = {
+        # Other config here
+        format = "$all"; # Remove this line to disable the default prompt format
+        palette = "catppuccin_${flavour}";
+      } // builtins.fromTOML (builtins.readFile
+        (pkgs.fetchFromGitHub
+          {
+            owner = "catppuccin";
+            repo = "starship";
+            rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc"; # Replace with the latest commit hash
+            sha256 = "sha256-soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
+          } + /palettes/${flavour}.toml));
+    };
 }
