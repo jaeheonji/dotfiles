e525ac5b8ef3eadb60e3a3336d07c4b2059d22b9
Jae-Heon Ji <atx6419@gmail.com>
2023-05-25 14:31:58 +0000

    :wrench: Change monitoring tools

--- home-manager/terminal/metric.nix
+++ home-manager/terminal/metric.nix
@@ -1,3 +1,19 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ zenith procs ];
+  home.packages = with pkgs; [ procs ];
+
+  home.file.".config/btop/themes/catppuccin_mocha.theme" = {
+    source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/btop/main/themes/catppuccin_mocha.theme";
+      hash = "sha256-KnXUnp2sAolP7XOpNhX2g8m26josrqfTycPIBifS90Y=";
+    };
+  };
+
+  programs.btop = {
+    enable = true;
+
+    settings = {
+      color_theme = "catppuccin_mocha.theme";
+      theme_background = false;
+    };
+  };
 }
