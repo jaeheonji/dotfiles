ccd4c86a76326d317b11d7af459797671752c0b4
Jae-Heon Ji <32578710+jaeheonji@users.noreply.github.com>
2023-06-09 11:41:43 +0900

    :wrench: Update hash of theme for btop

--- home-manager/terminal/metric.nix
+++ home-manager/terminal/metric.nix
@@ -4,7 +4,7 @@
   home.file.".config/btop/themes/catppuccin_mocha.theme" = {
     source = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/catppuccin/btop/main/themes/catppuccin_mocha.theme";
-      hash = "sha256-KnXUnp2sAolP7XOpNhX2g8m26josrqfTycPIBifS90Y=";
+      hash = "sha256-TeaxAadm04h4c55aXYUdzHtFc7pb12e0wQmCjSymuug=";
     };
   };
 
