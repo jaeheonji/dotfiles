052ca88d9622a6fd5fb6c58d807da835c988fdc4
Jae-Heon Ji <atx6419@gmail.com>
2023-07-05 00:45:43 +0900

    :wrench: Add zellij plugins

--- home-manager/terminal/zellij.nix
+++ home-manager/terminal/zellij.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }: {
+  # configuration
   home.file.".config/zellij" = {
     source = ../../config/zellij;
     recursive = true;
+  };
+
+  # monocle plugins for zellij
+  home.file.".local/share/zellij/plugins/monocle.wasm" = {
+    source = pkgs.fetchurl {
+      url = "https://github.com/imsnif/monocle/releases/download/0.37.2/monocle.wasm";
+      hash = "sha256-wEftVp/4b3WASxVun+p35Gr9KkobOSoqvg6kGXrztw4=";
+    };
   };
 
   programs.zellij = {
