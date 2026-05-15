70f8b48690cbdb5af50ad630bae8b301d35312e7
jaeheonji <atx6419@gmail.com>
2026-05-15 23:04:24 +0900

    feat: migrate hyprland config

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,16 @@
+-- This is an example Hyprland Lua config file.
+-- Refer to the wiki for more information.
+-- https://wiki.hypr.land/Configuring/Start/
+
+local custom = require("custom")
+
+-- Register custom layout before other configs to ensure it's available for rules and binds
+custom.layout.register()
+
+require("configs.envs")
+require("configs.execs")
+
+require("configs.general")
+require("configs.rules")
+
+require("configs.binds")
