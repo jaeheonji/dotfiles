d3f96a680bbc29bfe229307eb923cdedfca05311
jaeheonji <atx6419@gmail.com>
2026-08-14 13:15:44 +0900

    chore(nvim): update new plugins

--- .config/nvim/lua/plugins/default.lua
+++ .config/nvim/lua/plugins/default.lua
@@ -49,24 +49,4 @@ return {
       },
     },
   },
-
-  {
-    "christoomey/vim-tmux-navigator",
-    cond = vim.env.TERM_PROGRAM == "tmux",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
 }
